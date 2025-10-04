3a9ce8d47507d91541c4b151986017ec24c8e23e
haiph1210 <haiph12102001@gmail.com>
2025-10-05 00:16:54 +0700

    haipq3 - add favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import AppRouter from "../router/AppRouter.jsx";
+import { Helmet } from "react-helmet";
+import {asset} from "./utils/Assets.jsx";
 
 const App = () => {
-
+    const pageTitle = "Portfolio - Pham Quang Hai";
+    const faviconUrl = asset("/imgs/favicon.png")
 
     return (
         <div>
-            <AppRouter />
+            <Helmet>
+                <title>{pageTitle}</title>
+                <link rel="icon" type="image/svg+xml" href={faviconUrl} />
+            </Helmet>
+            <AppRouter/>
         </div>
     );
 };
